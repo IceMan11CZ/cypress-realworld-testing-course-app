eb41e3080f27537858748e9b64af3fe8ef963cac
Přemysl Unger <98833987+IceMan11CZ@users.noreply.github.com>
2023-07-18 15:15:00 +0200

    Test - git

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -11,7 +11,7 @@ describe('home page', () => {
   })
 
   it('the features on the homepage are correct', () => {
-  cy.get ("dt").eq(0).contains('4 Courses')
+  cy.get ("dt").eq(0).contains('4 Courses');
   })
 })
 
